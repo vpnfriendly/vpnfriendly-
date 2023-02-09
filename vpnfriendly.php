b2b7d24533c6d2e8d165d2676429c6076fea7961
vpnfriendly <124859063+vpnfriendly@users.noreply.github.com>
2023-02-09 20:15:55 +0700

    Update vpnfriendly.php

--- vpnfriendly.php
+++ vpnfriendly.php
@@ -1,6 +1,9 @@
  {
 "Version_update": "1.1",
-"Link_zip":"ใส่ลิงค์ไฟล์zip",
-"Password": "ตั้งรหัสผ่าน",
-"Message": "เขียนรายละเอียด"}
+"Link_zip":"https://github.com/vpnfriendly/vpnfriendly-/raw/main/vpnfriendly.zip",
+"Password": "249297",
+"Message": "ยินดีต้อนรับสู่ vpn friendly
+รายการเซิร์ฟเวอร์
+BM/Inet 
+ใช้งานได้ทั้ง3เครือข่าย AIS true dtac"}
 //หากอัปเดตแล้วไฟล์ไม่มาให้เช็คคำนี้ ( "} )
