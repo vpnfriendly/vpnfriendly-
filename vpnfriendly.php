b4b9477f744a7da0ed78b62df78110e66429729e
vpnfriendly <124859063+vpnfriendly@users.noreply.github.com>
2023-03-12 20:27:43 +0700

    Update vpnfriendly.php

--- vpnfriendly.php
+++ vpnfriendly.php
@@ -1,9 +1,14 @@
  {
-"Version_update": "1.1",
+"Version_update": "1.2",
 "Link_zip":"https://github.com/vpnfriendly/vpnfriendly-/raw/main/vpnfriendly.zip",
 "Password": "249297",
-"Message": "ยินดีต้อนรับสู่ vpn friendly
+"Message": "ยินดีต้อนรับสู่ vpn friendly 
+แอปใหม่ไฟล์ใหม่ ยังไม่ผ่านการใช้งาน
 รายการเซิร์ฟเวอร์
-BM/Inet 
+TH-Inet 10 เซิร์ฟเวอร์
+VIP-CAT 10 เซิร์ฟเวอร์
+BM.     10 เซิร์ฟเวอร์
+
+
 ใช้งานได้ทั้ง3เครือข่าย AIS true dtac"}
 //หากอัปเดตแล้วไฟล์ไม่มาให้เช็คคำนี้ ( "} )
