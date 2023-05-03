03216f624434406289562e780efb14041de169a7
vpnfriendly <124859063+vpnfriendly@users.noreply.github.com>
2023-05-03 12:05:47 +0700

    Add files via upload

---
+++ vpnfriendly.php
@@ -0,0 +1,16 @@
+{
+"Version_update": "1.5",
+"Link_zip":"https://github.com/vpnfriendly/vpnfriendly-/raw/main/vpnfriendly.zip",
+"Password": "249297",
+"Message": "ยินดีต้อนรับสู่ vpn friendly
+03/05/66
+เพิ่มไฟล์ใหม่ให้ใช้งานเพิ่มอีก 5 เซิร์ฟ
+TH-Inet 11-15
+รายการเซิร์ฟเวอร์
+TH-Inet 15 เซิร์ฟเวอร์
+VIP-CAT 10 เซิร์ฟเวอร์
+BM.     10 เซิร์ฟเวอร์
+
+
+ใช้งานได้ทั้ง3เครือข่าย AIS true dtac"}
+//หากอัปเดตแล้วไฟล์ไม่มาให้เช็คคำนี้ ( "} )
